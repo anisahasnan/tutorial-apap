49278cacb8f73bca4993ad402000bd3029e17356
anisahasnan <anisa.hasna@ui.ac.id>
2020-12-08 21:44:14 +0700

    feat: add dark mode toggle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
   state = {
     favItems: [],
     toggle: false,
+    isDarkMode: false,
   }
   
   handleItemAdd = (item) => {
@@ -45,14 +46,28 @@ export default class App extends React.Component {
   }
 
   showFavorite = () => {
-    this.setState({toggle: !this.state.toggle})
+    this.setState({toggle: !this.state.toggle});
+  }
+
+  toggleMode = () => {
+    this.setState({isDarkMode: !this.state.isDarkMode});
+
+    const { isDarkMode } = this.state;
+    localStorage.setItem('isDarkMode', !isDarkMode);
+  }
+
+  componentDidMount = () => {
+    const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+    this.setState({ isDarkMode });
   }
 
   render() {
     const { favItems } = this.state;
 
     return (
-        <div className="container-fluid">
+      <div>
+      { this.state.isDarkMode ?
+        <div className="container-fluid" style={{backgroundColor: 'black', color:'white'}}>
           <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
           <p className="text-center text-secondary text-sm font-italic">
             (This is a <strong>class-based</strong> application)
@@ -61,6 +76,11 @@ export default class App extends React.Component {
           <div class="custom-control custom-switch text-center">
             <input type="checkbox" class="custom-control-input" id="customSwitches" onClick={this.showFavorite} /> 
             <label class="custom-control-label" for="customSwitches">Show Favorites</label>
+          </div>
+
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <input type="checkbox" checked={this.state.isDarkMode} id="customSwitchesdark2" onClick={this.toggleMode} /> 
+            <label for="customSwitchesdark2">Dark Mode</label>
           </div>
           
           <div className="container pt-3 ">
@@ -100,6 +120,62 @@ export default class App extends React.Component {
             </div>
           </div>
         </div>
+        :
+          <div className="container-fluid" style={{backgroundColor: 'white', color:'black'}}>
+            <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
+            <p className="text-center text-secondary text-sm font-italic">
+              (This is a <strong>class-based</strong> application)
+            </p>
+  
+            <div class="custom-control custom-switch text-center">
+              <input type="checkbox" class="custom-control-input" id="customSwitches" onClick={this.showFavorite} /> 
+              <label class="custom-control-label" for="customSwitches">Show Favorites</label>
+            </div>
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <input type="checkbox" checked={this.state.isDarkMode} id="customSwitcheslight2" onClick={this.toggleMode} /> 
+              <label for="customSwitcheslight2">Dark Mode</label>
+            </div>
+            
+            <div className="container pt-3 ">
+              <div className="row">
+                <div className="col-sm ">
+                  <List
+                      title="List Movies"
+                      items={listMovies}
+                      onItemClick={this.handleItemAdd}
+                  />
+                </div>
+                
+                { this.state.toggle &&
+                  <div className= "col-sm">
+                    <div className="mb-2">
+                        {favItems.length > 0 &&
+                          <div>
+                            <button onClick={this.removeAllItem}>Delete All Favorite Items</button>
+                          </div>
+                        }
+                    </div>
+                            
+                    <List
+                      title= "My Favorites"
+                      items= {favItems}
+                      onItemClick={this.handleItemRemove}
+                    />
+  
+                    {favItems.length <= 0 &&
+                      <div className="text-center mt-2"> 
+                          <h3>Belum ada item yang dipilih</h3>
+                          <h4>Klik salah satu item di list movies</h4>
+                      </div>
+                    }
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
+          }
+      </div>
     );
   }
 }
